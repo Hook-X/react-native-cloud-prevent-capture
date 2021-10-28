ccd80d3f08fc01f07f2b7263b20e34b51bcec55e
Timur Davletshin <tdavletshin@cloudfactory.ru>
2021-10-28 10:55:25 +0300

    Minor changes

--- ios/CloudPreventCapture.m
+++ ios/CloudPreventCapture.m
@@ -1,7 +1,7 @@
 #import <React/RCTBridgeModule.h>
-#import "React/RCTEventEmitter.h"
+#import <React/RCTEventEmitter.h>
 
-@interface RCT_EXTERN_MODULE(CloudPreventCapture, NSObject)
+@interface RCT_EXTERN_MODULE(CloudPreventCapture, RCTEventEmitter)
 
 RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
                  withResolver:(RCTPromiseResolveBlock)resolve
